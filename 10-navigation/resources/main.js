a16b70d4478b0ac3963372fa4a352f77f66a1ebe
stef11111 <84786481+stef11111@users.noreply.github.com>
2021-07-21 02:06:28 +0300

    1

--- 10-navigation/resources/main.js
+++ 10-navigation/resources/main.js
@@ -5,30 +5,38 @@ const logo = {
     account_settings: "elite"
 };
 
-document.querySelectorAll("aside nav h2").forEach(e => {
-    e.addEventListener("click", function () {
-        if(e.id === "sign-out") return;
-        //set page
-        pageReset();
-        let pageIndex = this.innerHTML.replace(" ", "-").toLowerCase();
-        document.querySelector("aside nav img").src = "./assets/logo-" + 
-        logo[pageIndex.replace("-", "_")] + ".png";
-        document.querySelector('.' + pageIndex).style.zIndex = "1";
-        //set menu
-        menuReset();
-        e.classList.add("navSelected");
-    })    
-});    
+function pageReset () {
+    document.querySelectorAll(".page").forEach(e => { e.style.zIndex = "-1"; });    
+}
 
 document.querySelector("#order-details-link").addEventListener("click", function() {
     this.style.zIndex = "1";
     pageReset();
 })
 
-function pageReset () {
-    document.querySelectorAll(".page").forEach(e => { e.style.zIndex = "-1"; });    
-}
-
 function menuReset () {
     document.querySelectorAll("aside nav h2").forEach(e => { e.classList.remove("navSelected") });    
 }
+
+let container = document.querySelector(".container");
+function containerHeight () {
+    container.style.height = document.body.scrollHeight + "px";
+}
+containerHeight();
+
+document.querySelectorAll("aside nav h2").forEach(e => {
+    e.addEventListener("click", function () {
+        if(e.id === "sign-out") return;
+        //set page
+        pageReset();
+        let pageIndex = this.innerHTML.replace(" ", "-").toLowerCase();
+        document.querySelector("aside nav img").src = "./assets/logo-" +  
+        logo[pageIndex.replace("-", "_")] + ".png";
+        document.querySelector('.' + pageIndex).style.zIndex = "1";
+        containerHeight();
+        //set menu
+        menuReset();
+        e.classList.add("navSelected");
+    })    
+});    
+
