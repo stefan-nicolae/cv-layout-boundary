b58f8caf15bbd0941d6d3509e2d7f63a062bceb5
stef11111 <84786481+stef11111@users.noreply.github.com>
2021-07-21 04:09:52 +0300

    insanity

--- 10-navigation/resources/main.js
+++ 10-navigation/resources/main.js
@@ -4,9 +4,43 @@ const logo = {
     payment_methods: "member",
     account_settings: "elite"
 };
+let container = document.querySelector(".container");
+let aside = document.querySelector("aside");
+let currentPage = "your-orders";
 
 function pageReset () {
     document.querySelectorAll(".page").forEach(e => { e.style.zIndex = "-1"; });    
+}
+
+function menuReset () {
+    document.querySelectorAll("aside nav h2").forEach(e => { e.classList.remove("navSelected") });    
+}
+
+//in mobile: set the container height to the document height so the pages fill it 
+function containerHeight () {
+    if (window.innerWidth <= 900) { 
+        container.style.height = document.body.scrollHeight + "px";
+    } else {
+        container.style.height = "unset";
+    }
+}
+
+//in desktop: set the aside bar to fill the scroll height when there is a vertical scrollbar
+function insanity () {
+    //Vertical Scrollbar
+    var vs = container.scrollHeight > container.clientHeight;
+    if(vs) {
+        aside.style.height = container.scrollHeight + "px";
+    }
+    else {
+        aside.style.height = "100vh";
+    }
+}
+containerHeight();
+
+window.onresize = () => {
+    containerHeight();
+    insanity();
 }
 
 document.querySelector("#order-details-link").addEventListener("click", function() {
@@ -14,29 +48,20 @@ document.querySelector("#order-details-link").addEventListener("click", function
     pageReset();
 })
 
-function menuReset () {
-    document.querySelectorAll("aside nav h2").forEach(e => { e.classList.remove("navSelected") });    
-}
-
-let container = document.querySelector(".container");
-function containerHeight () {
-    container.style.height = document.body.scrollHeight + "px";
-}
-containerHeight();
-
 document.querySelectorAll("aside nav h2").forEach(e => {
     e.addEventListener("click", function () {
         if(e.id === "sign-out") return;
         //set page
         pageReset();
         let pageIndex = this.innerHTML.replace(" ", "-").toLowerCase();
+        currentPage = pageIndex;
         document.querySelector("aside nav img").src = "./assets/logo-" +  
         logo[pageIndex.replace("-", "_")] + ".png";
         document.querySelector('.' + pageIndex).style.zIndex = "1";
         containerHeight();
+        // insanity();
         //set menu
         menuReset();
         e.classList.add("navSelected");
     })    
 });    
-
